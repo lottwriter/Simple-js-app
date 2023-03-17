2902f55ab8e096cfa4484e4754fefbf14d2f1ae2
lettucemayoandonion <lottwriter@gmail.com>
2023-03-17 16:07:59 -0600

    Revisions
    Removed the old, uneeded scripts.js file and move the new scripts.js file to the 'js' folder.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = "Escargot";
-document.write(favoriteFood)
+let pokemonList = [
+    {name: 'Hitmonchan', height: 7, types: ['fighting']},
+    {name: 'Emolga', height: 3, types: ['electric', 'flying']}, 
+    {name: 'Lucario', height: 7, types: ['fighting', 'steel']}
+];
--- scripts.js
+++
@@ -1,1 +0,0 @@
-let pokemonList = [{name: 'Hitmonchan', height: 7, types: ['fighting']},{name: 'Emolga', height: 3, types: ['electric', 'flying']}, {name: 'Lucario', height: 7, types: ['fighting', 'steel']}];
