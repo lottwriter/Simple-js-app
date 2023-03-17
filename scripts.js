a50a5cdd3d30e0a9dd1d33fefada059829635b4c
lettucemayoandonion <lottwriter@gmail.com>
2023-03-16 21:42:42 -0600

    Update scripts.js
    Added an array with 3 objects, one for each pokemon (Hitmonchan, Emolga, Lucario)

--- scripts.js
+++ scripts.js
@@ -1,2 +1,1 @@
-let myName = 'Bob';
-document.write(myName);
+let pokemonList = [{name: 'Hitmonchan', height: 7, types: ['fighting']},{name: 'Emolga', height: 3, types: ['electric', 'flying']}, {name: 'Lucario', height: 7, types: ['fighting', 'steel']}];
