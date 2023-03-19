9945acf645949d307a9ffe68d71ae6855ef5886a
lettucemayoandonion <lottwriter@gmail.com>
2023-03-19 03:08:22 -0600

    Added code to scripts.js
    Added a loop to write information to the DOM based off the pokemonList entries.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ let pokemonList = [
     {name: 'Emolga', height: 3, types: ['electric', 'flying']}, 
     {name: 'Lucario', height: 7, types: ['fighting', 'steel']}
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `); //Adds pokemon names and height to the DOM in the form of text
+    if (pokemonList[i].height > 5) {
+        document.write(" - Tall fella. ")
+    }
+}
