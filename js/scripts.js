91e778a860d47f269fd24604a00e1a3994e4c424
lettucemayoandonion <lottwriter@gmail.com>
2023-03-25 01:55:24 -0600

    Added IIFE
    -Changed the for loop to a forEach() function.
    -Wrapped PokemonList in an IIFE to localize the variables.
    - Made the IIFE return two functions, add and getAll
    -Moved the forEach() function outside the IIFE, and modified it to account for the deglobalization of pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
-let pokemonList = [
-    {name: 'Hitmonchan', height: 7, types: ['fighting']},
-    {name: 'Emolga', height: 3, types: ['electric', 'flying']}, 
-    {name: 'Lucario', height: 7, types: ['fighting', 'steel']}
-];
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `); //Adds pokemon names and height to the DOM in the form of text
-    if (pokemonList[i].height > 5) {
-        document.write(" - Tall fella. ")
+let pokemonRepository = (function(){
+    let pokemonList = [
+        {name: 'Hitmonchan', height: 7, types: ['fighting']},
+        {name: 'Emolga', height: 3, types: ['electric', 'flying']}, 
+        {name: 'Lucario', height: 7, types: ['fighting', 'steel']}
+    ];
+
+
+return {
+    add: function(pokemon) {
+        pokemonList.push(pokemon);
+    },
+    getAll: function() {
+        return pokemonList;
     }
 }
+
+})();
+//return pokemonRepository.getAll()
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(`${pokemon.name} (height: ${pokemon.height}) `); //Adds pokemon names and height to the DOM in the form of text
+    if (pokemon.height > 5) {
+        document.write(" - Tall fella. ")
+    }
+})
