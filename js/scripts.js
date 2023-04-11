714a62c0c0593fe14b43efe67b9747d3e495acf1
lettucemayoandonion <lottwriter@gmail.com>
2023-04-10 23:13:05 -0600

    modals
    Added responsive modal functionality for every PokeDex entry!

--- js/scripts.js
+++ js/scripts.js
@@ -34,11 +34,62 @@ function add(pokemon) {
       console.error(e);
     });
   }
+
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+  }
+
   function showDetails(pokemon) {
-        loadDetails(pokemon).then(function () {
-          console.log(pokemon);
-        });
+    loadDetails(pokemon).then(function() {
+      let modalContainer = document.querySelector('#modal-container');
+      modalContainer.innerHTML = '';
+
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+      
+      // Add the new modal content
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = pokemon.name;
+    
+      let contentElement = document.createElement('p');
+      contentElement.innerText = `${pokemon.name} is ${pokemon.height} units tall.`;
+
+      let modalImg = document.createElement('img');
+      modalImg.src=`${pokemon.imageUrl}`;
+
+      
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modal.appendChild(modalImg);
+      modalContainer.appendChild(modal);
+      
+      modalContainer.classList.add('is-visible');}
+      
+      )
       }
+      let modalContainer = document.querySelector('#modal-container');
+      modalContainer.addEventListener('click', (e) => {
+        // Since this is also triggered when clicking INSIDE the modal
+        // We only want to close if the user clicks directly on the overlay
+        let target = e.target;
+        if (target === modalContainer) {
+          hideModal();
+        }
+      })
+      window.addEventListener('keydown', (e) => {
+        
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+          hideModal();  
+        }
+        ;
+      });
+      
 return {
     add: add,
     getAll: function() {
@@ -70,7 +121,6 @@ return {
 }
 
 })();
-
 //return pokemonRepository.getAll()
 pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon);
